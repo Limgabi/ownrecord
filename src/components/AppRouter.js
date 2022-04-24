3a7fecbdd2f0aa02347c0ea62f22c1ba59f00cf0
임가비 <gabi520@naver.com>
2022-04-25 00:28:01 +0900

    setup

---
+++ src/components/AppRouter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+import Profile from "../routes/Profile";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <BrowserRouter>
+            <Routes>
+                {isLoggedIn ? (
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </>
+                ) : (
+                    <Route path="/" element={<Auth />} />
+                )}
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter
